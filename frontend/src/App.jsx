bc83765ce1904962bf119774790b000cc88f1af2
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2025-07-10 21:45:01 +0530

    homepage rendered in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Button } from './components/ui/button'
+import HomePage from './pages/HomePage'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+      <> 
+  <HomePage/>
+  </>
+
   )
 }
 
