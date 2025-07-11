1bff38febec6a77f73e11fff1aef19e1095b5b90
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2025-07-11 23:56:23 +0530

    user model added

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userSchema=new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
