cd271796e7629c93ffc391ca43ad7f67009a5caa
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2025-07-12 22:15:24 +0530

    added gravatar

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -14,4 +14,24 @@ const userSchema=new Schema({
         type:String,
         required:true
     }
-})
+    ,
+    avatar:{
+        type:String,
+        required:false,
+        default:function(){
+            return getGravatarUrl(this.email);
+        },
+    },
+});
+
+function getGravatarUrl(email)
+{
+    const hash=require("crypto")
+    .createHash(md5)
+    .update(email.trim().toLowerCase())
+    .digest('hex');
+    return 'https://www.gravatar.com/avatar/${hash}?d=mp'>;
+
+}
+const User=mongoose.model("User",userSchema);
+export default User;
