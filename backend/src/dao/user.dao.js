ae24a84b8a6d8254d61b1f00d7807cf59784c72f
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2025-07-15 23:57:19 +0530

    user dao

---
+++ backend/src/dao/user.dao.js
@@ -0,0 +1,43 @@
+import User from '../models/user.model.js';
+
+export const findUserByEmail=async (email)=>{
+    return await User.findOne({email});
+}
+
+
+export const findUserById=async (id)=>{
+    return await User.findUserById(id);
+}
+
+export const findUserByEmailByPassword = async (email) => {
+    return await User.findOne({email}).select('+password')
+}
+
+export const createUser=async (name,email,password)=>{
+    const newUser=new User({name,email,password});
+    await newUser.save();
+    return newUser;
+}
+
+export const updateUser =async (id,name,password,email)=>{
+    const user=await User.findUserById(id);
+    if(!user) throw new Error("User not Found");
+    user.name=name;
+    user.email=email;
+    user.password=password;
+    await User.save();
+    return user;
+}
+
+export const deleteUser =async (id)=>{
+    const user=await User.findUserById(id);
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  await user.deleteOne();
+  return { message: 'User deleted successfully' };
+
+
+}
+
