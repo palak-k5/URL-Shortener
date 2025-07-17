068e4410b20e4d7b10e05c5f307563918948404c
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2025-07-18 02:07:51 +0530

    redux setup

---
+++ frontend/src/store/slice/authSlice.js
@@ -0,0 +1,30 @@
+
+
+import  {createSlice} from '@reduxjs/toolkit';
+
+const initialState={
+    user:null,
+    isAuthenticated:false,
+    
+};
+
+const authSlice=createSlice(
+    {
+        name:'auth',
+        initialState,
+        reducers:{
+            login:(state,action)=>{
+                state.user=action.payload;
+                state.isAuthenticated=true;
+            }
+            ,
+            logout: (state)=>{
+                state.user=null.action;
+                state.isAuthenticated=false;
+            }
+        },
+    }
+)
+
+export const {clearError}=authSlice.actions;
+export default authSlice.reducer;
