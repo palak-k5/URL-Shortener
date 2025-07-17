fcc6bd0b86e693ee61b4d6e6c622e5d43bb842ab
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2025-07-18 02:09:55 +0530

    tanstack updates in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Button } from './components/ui/button'
 import HomePage from './pages/HomePage'
+import { Outlet } from '@tanstack/react-router'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
       <> 
-  <HomePage/>
+      <NavBar/>
+      <Outlet/>
   </>
 
   )
