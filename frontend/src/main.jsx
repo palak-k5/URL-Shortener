565276a81270eae5918262f61f659d3b1e579eac
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2025-07-18 02:09:03 +0530

    until redux,tanstack router setup updates in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { QueryClientProvider,QueryClient } from '@tanstack/react-query'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree} from './routing/routeTree.js'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
+const queryClient=new QueryClient();
+const router = createRouter({ routeTree})
 
 createRoot(document.getElementById('root')).render(
-    <App />,
+    <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+        <RouterProvider router ={router}/>
+    </QueryClientProvider>
+    </Provider>
 )
