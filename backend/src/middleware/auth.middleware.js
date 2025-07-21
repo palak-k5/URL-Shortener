872966e798f423e439596ab17125e475139d9032
Palak Kumrawat <100283614+palak-k5@users.noreply.github.com>
2025-07-21 22:34:31 +0530

    auth.middleware.js

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,6 +1,6 @@
 import { findUserById } from "../dao/user.dao.js"
 import { verifyToken } from "../utils/helper.js"
-
+//middleware
 export const authMiddleware = async (req, res, next) => {
     const token = req.cookies.accessToken
     if(!token) return res.status(401).json({message:"Unauthorized"})
