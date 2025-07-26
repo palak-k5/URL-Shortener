02797b4d91d7e5b40b12050ce5514a3a268361cc
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2025-07-27 01:10:04 +0530

    check if authenticated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,19 @@ import { Button } from './components/ui/button'
 import HomePage from './pages/HomePage'
 import { Outlet } from '@tanstack/react-router'
 import NavBar from './components/NavBar'
+import { useSelector } from 'react-redux'
+import { Toaster } from './components/ui/sonner'
 
 const App = () => {
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
   return (
       <> 
-      <NavBar/>
+      {!isAuthenticated && <NavBar />}
+            <Toaster richColors />
+
       <Outlet/>
+
   </>
 
   )
