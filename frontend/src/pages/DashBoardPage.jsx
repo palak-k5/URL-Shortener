cfb3195d16c911d481b63a4085c51ba1b450551e
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2025-07-27 01:05:44 +0530

    dashboard page,table,route

---
+++ frontend/src/pages/DashBoardPage.jsx
@@ -0,0 +1,24 @@
+import FireflyBackground from '../components/FireFlyBackground'
+import UrlForm from '../components/UrlForm'
+import UserUrls from '../components/UserUrl'
+import Navbar from '../components/Navbar' // Import your Navbar component
+import React from 'react'
+
+const DashBoardPage = () => {
+  return (
+    <div className="relative min-h-screen w-full">
+      <FireflyBackground />
+
+      {/* Navbar at the top */}
+      <Navbar appName="URL Shortener" />
+
+      {/* Main content */}
+      <div className="relative z-10 flex flex-col items-center justify-start w-full pt-16 md:pt-20">
+        <UrlForm />
+        <UserUrls />
+      </div>
+    </div>
+  )
+}
+
+export default DashBoardPage
