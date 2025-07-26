41577119373094621b1fb26f5844cadad67a96b3
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2025-07-27 01:09:37 +0530

    tanstack

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,11 @@ import { routeTree} from './routing/routeTree.js'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
 const queryClient=new QueryClient();
-const router = createRouter({ routeTree})
+const router = createRouter({ routeTree,
+    context:{
+        queryClient,store
+    }
+})
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
